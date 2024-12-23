1015302f85696c78b8d225032a3c3f0f9bb59d1e
JoSantana0005 <joseasantana05@gmail.com>
2024-12-23 13:56:31 -0400

    Un aplausooo

--- src/Js/Book.js
+++ src/Js/Book.js
@@ -1,1 +1,24 @@
+// Agregamos las peliculas a la pagina
 const Local = localStorage.getItem('Favorites');
+const Favorites = JSON.parse(Local);
+console.log(Favorites);
+
+let Serie = '';
+
+// Function para agregar las peliculas favoritas
+function FavoriteMovie(){
+    let Movie = '';
+    Favorites.forEach(favorite => {
+        Movie += `<div class="Movies--List">
+                        <img src="${favorite.Poster}" alt="Logo">
+                        <div>
+                            <span id="Fecha--Movie">${favorite.Fecha}</span>
+                            <span id="Catalogo-Movie">${favorite.catalog}</span>
+                            <span id="Tipo--Movie">${favorite.tipo}</span>
+                        </div>
+                        <h4>${favorite.Name}</h4>
+                    </div>`;
+    });
+    document.getElementById('List--Movies').innerHTML = Movie;
+}
+FavoriteMovie();
