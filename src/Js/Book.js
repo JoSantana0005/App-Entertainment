a955df62204e6e47af63c48322b31f03e191a7dd
JoSantana0005 <joseasantana05@gmail.com>
2024-12-24 10:00:25 -0400

    Funcion de la barra de busqueda lista

--- src/Js/Book.js
+++ src/Js/Book.js
@@ -10,11 +10,32 @@ const Popularity = document.getElementById('Popularity');
 const Overview = document.getElementById('Overview');
 const Ventana_modal = document.getElementById('Ventana-Modal');
 const cerrar_modal = document.getElementById('Cerrar');
+const ContainerMovie = document.getElementsByClassName('Movies')[0];
+const ContainerSerie = document.getElementsByClassName('Series')[0];
+const Title = document.getElementById('Title');
 console.log(Favorites);
 // Evento para cerrar la ventana modal
 cerrar_modal.addEventListener('click',()=>{
     Ventana_modal.close();
 })
+// Funcion para restablecer el contenido de las sections
+function actualizarSection(){
+    ContainerMovie.innerHTML = `<h2 id="Title">Bookmarked Movies</h2>
+            <article class="List--Favorites--Movies" id="List--Favorites--Movies">
+                <div class="List--Movies" id="List--Movies"></div>
+            </article>`
+    ContainerSerie.innerHTML = `<h2>Bookmarked TV</h2>
+            <article class="List--favorites--Series" id="List--favorites--Series">
+                <div class="List--Series" id="List--Series"></div>
+            </article>`
+}
+// Fucntion para colocar los resultados del buscador en una section
+function SearchResults(){
+    ContainerMovie.innerHTML = `<h2 id="Title">${Title.innerText}</h2>
+            <article class="List--Favorites--Movies" id="List--Favorites--Movies">
+                <div class="List--Movies" id="List--Movies"></div>
+            </article>`
+}
 // Function para agregar las series a favoritos
 function FavoriteSerie(){
     let Serie = '';
@@ -92,5 +113,59 @@ function FavoriteMovie(){
     })
 
 }
+// Function para la busqueda de la pelicula o series
+const Search = document.getElementById('Search--Favorite');
+Search.addEventListener('input',(e)=>{
+    const value = e.target.value.trim();
+    console.log(value);
+    if(value == ''){
+        actualizarSection();
+        FavoriteMovie();
+        FavoriteSerie();
+    }else{
+        Title.innerText = 'Founds the results';
+        SearchResults();
+        document.getElementsByClassName('Series')[0].innerHTML = '';
+        Favorites.forEach((favorite,index) =>{
+            const isVisble = favorite.Name.toLowerCase().includes(value.toLowerCase());
+            if(isVisble){
+                List = `<div class="Movies--List" data-index=${index}>
+                        <img src="${favorite.Poster}" alt="Logo">
+                        <div>
+                            <span id="Fecha--Movie">${favorite.Fecha}</span>
+                            <span id="Catalogo-Movie">${favorite.catalog}</span>
+                            <span id="Tipo--Movie">${favorite.tipo}</span>
+                        </div>
+                        <h4>${favorite.Name}</h4>
+                    </div>`;
+            }
+        })
+        if(List != ''){
+            document.getElementById('List--Movies').innerHTML = List
+            // Ventana Modal
+            const List_Search = document.querySelectorAll('.Movies--List');
+            List_Search.forEach(element =>{
+                element.addEventListener('click',()=>{
+                    const index = element.getAttribute('data-index');
+                    if(Ventana_modal){
+                        Ventana_modal.showModal();
+                        Imagen.src = Favorites[index].Poster;
+                        Title_dialog.textContent = Favorites[index].Name;
+                        Fecha_dialog.textContent = Favorites[index].Fecha;
+                        catalogo.textContent = Favorites[index].catalog;
+                        Tipo_movie.textContent = Favorites[index].tipo
+                        Popularity.textContent = Favorites[index].popularidad;
+                        Overview.textContent = Favorites[index].descripcion;
+                    }else{
+                        console.log("No existen la ventana");
+                    }
+                })
+            })
+        }else{
+            document.getElementById('List--Movies').innerHTML = '<p>No hay</p>';
+        }
+    }
+})
 FavoriteMovie();
 FavoriteSerie();
+
